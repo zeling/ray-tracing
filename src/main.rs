f92cb13d67a3cf88c4f97a8c6c3fc0dfb291cf1d
Zeling Feng <zeling.feng@gmail.com>
2022-10-09 22:10:12 -0700

    Sphere renderer

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,53 @@ impl Renderer for GradientRenderer {
                     orig: origin,
                     dir: lower_left_corner + u * horizontal + v * vertical - origin,
                 };
-                let color = ray.color();
+                let color = {
+                    let dir = glm::normalize(&ray.dir);
+                    let t = 0.5 * (dir[1] + 1.0);
+                    Color((1.0 - t) * glm::vec3(1.0, 1.0, 1.0) + t * glm::vec3(0.0, 0.0, 1.0))
+                };
+                color.write(out)?;
+            }
+        }
+        out.flush()
+    }
+}
+
+struct SphereRenderer(Sphere);
+
+impl Renderer for SphereRenderer {
+    fn render<W: std::io::Write>(&self, out: &mut W) -> Result<(), std::io::Error> {
+        const ASPECT_RATIO: f32 = 16.0 / 9.0;
+        const IMAGE_WIDTH: u32 = 400;
+        const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as u32;
+
+        const VIEWPORT_HEIGHT: f32 = 2.0;
+        const VIEWPORT_WIDTH: f32 = VIEWPORT_HEIGHT * ASPECT_RATIO;
+        const FOCAL_LENGTH: f32 = 1.0;
+
+        let origin = glm::vec3(0.0, 0.0, 0.0);
+        let horizontal = glm::vec3(VIEWPORT_WIDTH, 0.0, 0.0);
+        let vertical = glm::vec3(0.0, VIEWPORT_HEIGHT, 0.0);
+        let lower_left_corner =
+            origin - horizontal * 0.5 - vertical * 0.5 - glm::vec3(0.0, 0.0, FOCAL_LENGTH);
+        write!(out, "P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT)?;
+        for j in (0..IMAGE_HEIGHT).rev() {
+            for i in 0..IMAGE_WIDTH {
+                let u = (i as f32) / (IMAGE_WIDTH as f32 - 1.0);
+                let v = (j as f32) / (IMAGE_HEIGHT as f32 - 1.0);
+                let ray = Ray {
+                    orig: origin,
+                    dir: lower_left_corner + u * horizontal + v * vertical - origin,
+                };
+                let color = {
+                    if self.0.hits_by(&ray) {
+                        Color(glm::vec3(1.0, 0.0, 0.0))
+                    } else {
+                        let dir = glm::normalize(&ray.dir);
+                        let t = 0.5 * (dir[1] + 1.0);
+                        Color((1.0 - t) * glm::vec3(1.0, 1.0, 1.0) + t * glm::vec3(0.0, 0.0, 1.0))
+                    }
+                };
                 color.write(out)?;
             }
         }
@@ -55,7 +101,11 @@ impl Renderer for GradientRenderer {
 }
 
 fn main() -> Result<(), std::io::Error> {
-    GradientRenderer.render(&mut std::io::stdout().lock())
+    let renderer = SphereRenderer(Sphere {
+        center: glm::vec3(0.0, 0.0, -1.0),
+        radius: 0.5,
+    });
+    renderer.render(&mut std::io::stdout().lock())
 }
 
 struct Ray {
@@ -81,10 +131,20 @@ impl Ray {
     fn at(&self, t: f32) -> glm::Vec3 {
         self.orig + t * self.dir
     }
+}
 
-    fn color(&self) -> Color {
-        let dir = glm::normalize(&self.dir);
-        let t = 0.5 * (dir[1] + 1.0);
-        Color((1.0 - t) * glm::vec3(1.0, 1.0, 1.0) + t * glm::vec3(0.0, 0.0, 1.0))
+struct Sphere {
+    center: glm::Vec3,
+    radius: f32,
+}
+
+impl Sphere {
+    fn hits_by(&self, ray: &Ray) -> bool {
+        let oc = ray.orig - self.center;
+        let a = glm::dot(&ray.dir, &ray.dir);
+        let b = glm::dot(&ray.dir, &oc) * 2.0;
+        let c = glm::dot(&oc, &oc) - self.radius * self.radius;
+        let delta = b * b - 4.0 * a * c;
+        delta >= 0.0
     }
 }
